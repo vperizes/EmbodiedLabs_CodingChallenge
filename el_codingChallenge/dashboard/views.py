c6df481c3a84c3450790ce0e7fb523d7718ef363
vperizes <vperizes@gmail.com>
2024-06-03 21:08:45 -0500

    add get request to api. create html to render api response

--- el_codingChallenge/dashboard/views.py
+++ el_codingChallenge/dashboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 def userProfileDashboard(request):
-    return HttpResponse("Hello! Is this working?")
+    base_url = "https://randomuser.me/api/"
+    api_response = requests.get(base_url, params={"results": 5})
+    users = api_response.json()
+
+    print(users)
+
+    return render(request, "dashboard/users.html", {"users": users})
