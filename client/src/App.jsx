321a8d53f91f5dd82d49743dc25b097fc7cab00a
vperizes <vperizes@gmail.com>
2024-06-04 01:22:52 -0500

    add basic logic to request data from backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+
+const data = await axios.get("http://127.0.0.1:8000/");
+console.log(data);
+
 const App = () => {
-  return <h1>User Profile Dashboard</h1>;
+  return (
+    <>
+      <h1>User Profile Dashboard</h1>
+    </>
+  );
 };
 export default App;
