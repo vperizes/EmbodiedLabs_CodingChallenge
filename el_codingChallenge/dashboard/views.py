c39038270b6115ded7fc19d601e34e5be0464dda
vperizes <vperizes@gmail.com>
2024-06-03 22:16:33 -0500

    add logic to simpligy response data from external api

--- el_codingChallenge/dashboard/views.py
+++ el_codingChallenge/dashboard/views.py
@@ -5,9 +5,22 @@ import requests
 
 def userProfileDashboard(request):
     base_url = "https://randomuser.me/api/"
-    api_response = requests.get(base_url, params={"results": 5})
+    api_response = requests.get(base_url, params={"results": 5, "nat": "us"})
     users = api_response.json()
 
-    print(users)
+    simplified_users = []
+    for user in users["results"]:
+        try:
+            simplified_user = {
+                "gender": user["gender"],
+                "first_name": user["name"]["first"],
+                "last_name": user["name"]["last"],
+                "email": user["email"],
+                "cell_phone": user["cell"],
+                "profile_pic": user["picture"]["medium"],
+            }
+            simplified_users.append(simplified_user)
+        except:
+            print("invalid data")
 
-    return render(request, "dashboard/users.html", {"users": users})
+    return render(request, "dashboard/users.html", {"users": simplified_users})
