d283f4380d2d29e2ec4db21af89601977d1c4054
vperizes <vperizes@gmail.com>
2024-06-05 23:27:45 -0500

    add raycaster to handle click

--- client/src/components/Cube.jsx
+++ client/src/components/Cube.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
-import * as THREE from "three";
 import { useAllUsersContext } from "../App";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeometry.js";
 
 const Cube = () => {
   const { users } = useAllUsersContext();
@@ -10,8 +12,8 @@ const Cube = () => {
     const height = 800;
 
     //initiliaze camera (fov, aspect ratio, near plane, far plane)
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    camera.position.z = 1;
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 20);
+    camera.position.z = 3;
 
     // set up scene
     const scene = new THREE.Scene();
@@ -21,7 +23,13 @@ const Cube = () => {
     const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
     renderer.setSize(width, height);
 
-    // Function to create a texture from text
+    //need to append renderer to DOM
+    document.body.appendChild(renderer.domElement);
+
+    //set up controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // Fn creates a canvas, draws text on it, and converts the canvas into a texture.
     const createTextTexture = (text) => {
       const canvas = document.createElement("canvas");
       const context = canvas.getContext("2d");
@@ -41,21 +49,21 @@ const Cube = () => {
 
     const materials = users.map((user) => {
       const name = `${user.first_name} ${user.last_name}`;
-      return new THREE.MeshBasicMaterial({
+      return new THREE.MeshStandardMaterial({
         map: createTextTexture(name),
       });
     });
 
     while (materials.length < 6) {
-      materials.push(new THREE.MeshBasicMaterial({ color: 0x000000 }));
+      materials.push(new THREE.MeshStandardMaterial({ color: 0x000000 }));
     }
 
     //create cube geo and mesh
-    const cube_geo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const cube = new THREE.Mesh(cube_geo, materials);
-    cube.rotation.x = 0.5;
-    cube.rotation.y = 0.5;
-    scene.add(cube);
+    const roundedCube_geo = new RoundedBoxGeometry(1, 1, 1, 4, 0.1);
+    const roundedCube = new THREE.Mesh(roundedCube_geo, materials);
+    roundedCube.rotation.x = 0.5;
+    roundedCube.rotation.y = 0.5;
+    scene.add(roundedCube);
 
     ///Lighting
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -66,10 +74,26 @@ const Cube = () => {
     spotLight.position.set(0, 0.5, 20);
     scene.add(spotLight);
 
-    renderer.render(scene, camera);
+    const pointer = new THREE.Vector2();
+    const raycaster = new THREE.Raycaster();
 
-    //need to append renderer to DOM
-    document.body.appendChild(renderer.domElement);
+    const onClickFace = (event) => {
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = (event.clientY / window.innerWidth) * 2 + 1;
+
+      raycaster.setFromCamera(pointer, camera);
+      const intersect = raycaster.intersectObject(materials);
+      console.log(intersect);
+    };
+
+    window.addEventListener("click", onClickFace);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    };
+    animate();
   };
 
   //pass in users as dependecy arr --> effect will run whenever users changes
